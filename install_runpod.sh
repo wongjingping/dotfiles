185c1c19229a38de8ea893dc59f6a3f23345d302
jp <wongjingping@gmail.com>
2024-03-26 10:33:43 +0800

    removed dotfile cloning for runpod

--- install_runpod.sh
+++ install_runpod.sh
@@ -10,6 +10,3 @@ git config --global core.sshCommand "ssh -i /root/.ssh/id_rsa_runpod"
 cd /root
 # add github's host ip to known hosts to avoid interactive prompt when cloning
 ssh-keyscan -H github.com >> .ssh/known_hosts
-# pull dot files
-git clone git@github.com:wongjingping/dotfiles.git
-cp dotfiles/zshrc_runpod .zshrc # no need to symlink since config is ephemeral
